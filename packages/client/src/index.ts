81c9f8a7b86907548454b6b5e7801cd70b24d07d
David Corticchiato <david.corticchiato@gmail.com>
2024-10-27 15:20:11 -0400

    hook first version

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -9,36 +9,56 @@ type Meta = {
   localPlayerId?: string;
 };
 
-function getPlayer<GameCustom, PlayerCustom>(playerId?: string, game?: Game<GameCustom, PlayerCustom>) {
+function getPlayer<GameCustom, PlayerCustom>(
+  playerId?: string,
+  game?: Game<GameCustom, PlayerCustom>
+) {
   if (!game || !playerId) {
     return undefined;
   }
 
-  return game.players.find(player => player.id == playerId);
+  return game.players.find((player) => player.id == playerId);
 }
 
-function isHost<GameCustom, PlayerCustom>(playerId?: string, game?: Game<GameCustom, PlayerCustom>) {
+function isHost<GameCustom, PlayerCustom>(
+  playerId?: string,
+  game?: Game<GameCustom, PlayerCustom>
+) {
   let player = getPlayer(playerId, game);
 
   return player?.host;
 }
 
-export function useRemoteState<GameCustom, PlayerCustom>(uri: string, gameId?: string) {
-  let [meta, setMeta] = useState<Meta>({ client: ioc(uri), isInitialized: false, isHostReady: false })
-  let [game, setGame] = useState<Game<GameCustom, PlayerCustom>>();
+type Reducer<GameCustom, PlayerCustom, Action> = (
+  game: Game<GameCustom, PlayerCustom>,
+  action: Action,
+  playerId: string
+) => Game<GameCustom, PlayerCustom>;
 
+export function useRemoteReducer<GameCustom, PlayerCustom, Action>(
+  uri: string,
+  reducer: Reducer<GameCustom, PlayerCustom, Action>,
+  gameId?: string
+): [Game<GameCustom, PlayerCustom> | undefined, string | undefined] {
+  let [meta, setMeta] = useState<Meta>({
+    client: ioc(uri),
+    isInitialized: false,
+    isHostReady: false,
+  });
+
+  let [game, setGame] = useState<Game<GameCustom, PlayerCustom>>();
 
   useEffect(() => {
     if (!meta.isInitialized) {
-      meta.client.on('connect', () => {
+      meta.client.on("connect", () => {
         if (gameId == undefined) {
-          meta.client.emit('create');
+          meta.client.emit("create");
         } else {
-          meta.client.emit('join', { gameId });
+          meta.client.emit("join", { gameId });
         }
       });
 
-      meta.client.on('update', (update) => {
+      meta.client.on("update", (update) => {
         if (!meta.localPlayerId && !!update.playerId) {
           setMeta({ ...meta, localPlayerId: update.playerId });
         }
@@ -48,12 +68,24 @@ export function useRemoteState<GameCustom, PlayerCustom>(uri: string, gameId?: s
 
       setMeta({ ...meta, isInitialized: true });
     } else if (isHost(meta.localPlayerId, game) && !meta.isHostReady) {
-      meta.client.on('join', join => {
-        meta.client.emit('accept', { gameId: game?.id, playerId: join.playerId });
-      })
+      meta.client.on("join", (join) => {
+        meta.client.emit("accept", {
+          gameId: game?.id,
+          playerId: join.playerId,
+        });
+      });
+
+      meta.client.on("notify", (notify) => {
+        if (!!game) {
+          meta.client.emit("update", {
+            game: reducer(game, notify.action, notify.playerId),
+          });
+        }
+      });
 
       setMeta({ ...meta, isHostReady: true });
     }
+  });
 
-  })
+  return [game, meta.localPlayerId];
 }
--- packages/client/tests/hook.test.tsx
+++ packages/client/tests/hook.test.tsx
@@ -2,12 +2,48 @@ import { expect, test, beforeEach, afterEach, assert } from "vitest";
 import { Server } from "@react-remote-state/server";
 import { io as ioc, type Socket as Client } from "socket.io-client";
 import { render, screen, fireEvent } from "@testing-library/react";
+import React from "react";
+import { useRemoteReducer } from "../src";
 
-test("foo", () => {
-  function ComponentTest() {
-    console.log("component");
-    return <div></div>;
-  }
+let server: Server;
 
-  render(<ComponentTest />);
+function createClient() {
+  return new Promise<Client>((done) => {
+    let client: Client = ioc(`http://localhost:${server.port}`);
+
+    client.on("connect", () => done(client));
+    client.on("error", (error) => console.error(error));
+  });
+}
+
+beforeEach(
+  () =>
+    new Promise<void>((done) => {
+      server = new Server();
+      server.start(done);
+    })
+);
+
+afterEach(async () => {
+  await server.stop();
 });
+
+test("create game when no gameId is provided", () =>
+  new Promise<void>((done) => {
+    function ComponentTest() {
+      let [game, playerId] = useRemoteReducer(
+        `http://localhost:${server.port}`,
+        (game, action, playerId) => game
+      );
+
+      if (!!game) {
+        expect(game.id).toBeTypeOf("string");
+        expect(game.players.length).toBe(1);
+        done();
+      }
+
+      return <div></div>;
+    }
+
+    render(<ComponentTest />);
+  }));
